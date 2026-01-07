8950907c3df41b04100d2a46c83bb38721c179d3
ganeshpishey <ganeshrameshpishey@gmail.com>
2026-01-07 12:44:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
+import React, { Suspense } from 'react'; 
 import './App.css';
 import Navbar from './components/Navbar/navbar';
-import Homescreen from './Screen/homescreen/homescreen';
-import {Routes, Route} from 'react-router-dom';
-import Product from './Screen/Product/product';
 import Footer from './components/Footer/footer';
-import Cart from './Screen/Cart/cart';
+import { Routes, Route } from 'react-router-dom';
+
+const Homescreen = React.lazy(() => import('./Screen/homescreen/homescreen'));
+const Product = React.lazy(() => import('./Screen/Product/product'));
+const Cart = React.lazy(() => import('./Screen/Cart/cart'));
+
 function App() {
   return (
-    <div className = "App">
-      <Navbar/>
-      <Routes>
-        <Route path ='/' element = {<Homescreen/>}/>
-        <Route path ='/product' element = {<Product/>} />
-        <Route path ='/cart' element = {<Cart/>} />
-      </Routes>
-      <Footer/>
+    <div className="App">
+      <Navbar />
       
+      {/* 4. Wrap Routes in Suspense to handle the loading state */}
+      <Suspense fallback={<div style={{textAlign: 'center', marginTop: '50px'}}>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Homescreen />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </Suspense>
+
+      <Footer />
     </div>
-  )   
+  );
 };
 
-export default App
+export default App;
